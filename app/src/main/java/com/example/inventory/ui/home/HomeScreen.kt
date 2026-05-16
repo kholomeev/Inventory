fe345772cbc353ccfeecc179e0c8dbd1f353bdc4
Kirill <holomeev2011@gmail.com>
2026-05-16 12:50:56 +0300

    шаг 2+

--- app/src/main/java/com/example/inventory/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/inventory/ui/home/HomeScreen.kt
@@ -39,6 +39,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
